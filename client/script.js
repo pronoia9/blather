d7abfdb0fe02d68dbcccc5a2d1303d3187f8eb09
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-01 21:34:17 +0300

    add handleSubmit function

--- client/script.js
+++ client/script.js
@@ -25,7 +25,8 @@ function typeText(element, text) {
 }
 
 function generateUniqueId() {
-  const timestamp = Date.now(), randomNumber = Math.random();
+  const timestamp = Date.now(),
+    randomNumber = Math.random();
   const hexadecimalString = randomNumber.toString(16);
   return `id-${timestamp}-${hexadecimalString}`;
 }
@@ -42,3 +43,26 @@ function chatStripe(isAi, value, uniqueId) {
   </div>
   `;
 }
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // users chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  form.reset();
+
+  // bots chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+  const messageDiv = document.getElementById(uniqueId);
+  loader(messageDiv);
+};
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) handleSubmit(e);
+});
