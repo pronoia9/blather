0ed2e7b037d319bd12a8cf67365c35a0636e0637
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-01 21:27:45 +0300

    add chatStripe function

--- client/script.js
+++ client/script.js
@@ -29,3 +29,16 @@ function generateUniqueId() {
   const hexadecimalString = randomNumber.toString(16);
   return `id-${timestamp}-${hexadecimalString}`;
 }
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+  <div class="wrapper${isAi && ' ai'}">
+    <div class="chat">
+      <div class="profile">
+        <img src="${isAi ? bot : user}" alt="${isAi ? 'bot' : 'user'}" />
+      </div>
+      <div class="message" id=${uniqueId}>${value}</div>
+    </div>
+  </div>
+  `;
+}
