717836cab237ee17d09d0d40a547b36031809d83
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-01 21:19:49 +0300

    add typeText function

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
-import bot from './assets/bot.svg'
-import user from './assets/user.svg'
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
@@ -12,4 +12,14 @@ function loader(element) {
   loadInterval = setInterval(() => {
     element.textContent !== '...' ? (element.textContent += '.') : (element.textContent = '');
   }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innetHTML += text.charAt(index);
+      index++;
+    } else clearInterval(interval);
+  }, 20);
 }
