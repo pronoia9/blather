61ef4133be95a4a9e8a9221851f1736273943b2a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-01 22:36:02 +0300

    fix url

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('https://codex-im0y.onrender.com/', {
+  const response = await fetch('http://localhost:5005/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ prompt: data.get('prompt') }),
