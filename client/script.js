bc2f752a282e5ce7585a5ba894e0ed2d48be2276
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-15 00:54:36 +0300

    add get date func

--- client/script.js
+++ client/script.js
@@ -81,3 +81,14 @@ form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => {
   if (e.keyCode === 13) handleSubmit(e);
 });
+
+function getDate(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
