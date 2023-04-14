daeefcf9f2e6661bdfcacd30453003652571ee78
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-15 01:46:34 +0300

    fix functionality

--- client/script.js
+++ client/script.js
@@ -1,33 +1,45 @@
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
-const form = document.querySelector('form');
+const form = document.querySelector('form.channel-message-form');
 const chatContainer = document.querySelector('#chat_container');
+
+console.log(form);
+console.log(chatContainer);
 
 let loadInterval;
 
+// The . . . writing message thing for AI reply
 function loader(element) {
+  console.log('LOADER()');
+  // element.textContent = '';
+  // loadInterval = setInterval(() => {
+  //   element.textContent !== '...' ? (element.textContent += '.') : (element.textContent = ' ');
+  // }, 300);
   element.textContent = '';
-
   loadInterval = setInterval(() => {
-    element.textContent !== '...' ? (element.textContent += '.') : (element.textContent = ' ');
+    // Update the text content of the loading indicator
+    element.textContent += '.';
+    // If the loading indicator has reached three dots, reset it
+    if (element.textContent === '....') element.textContent = '';
   }, 300);
 }
 
+// AI typing text letter by letter
 function typeText(element, text) {
+  console.log('TYPETEXT()');
   let index = 0;
   let interval = setInterval(() => {
-    if (index < text.length) {
-      element.innerHTML += text.charAt(index);
-      index++;
-    } else clearInterval(interval);
-  }, 20);
+    if (index < text.length) { element.innerHTML += text.charAt(index); index++; }
+    else clearInterval(interval);
+  }, 10);
 }
 
 // generate unique ID for each message div of bot
 // necessary for typing text effect for that specific reply
 // without unique ID, typing text will work on every element
 function generateUniqueId() {
+  console.log('GENERATEUNIQUEID()');
   const timestamp = Date.now(), randomNumber = Math.random();
   const hexadecimalString = randomNumber.toString(16);
   return `id-${timestamp}-${hexadecimalString}`;
@@ -35,18 +47,21 @@ function generateUniqueId() {
 
 const handleSubmit = async (e) => {
   e.preventDefault();
+  console.log('HANDLESUBMIT()');
 
+  console.log('form:', form);
   const data = new FormData(form);
+  console.log('data:', data);
 
   // user's chatstripe
-  chatContainer.innerHTML += `<div class='message message-personal'>${data.get('prompt')}</div>`;
+  chatContainer.innerHTML += `<div class='message'><div class='message__body'>${data.get('message')}</div><div class='message__footer'><span class='message__authoring'>User</span> - </div></div>`;
 
   // to clear the textarea input
   form.reset();
 
   // bot's chatstripe
   const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += `<div class='message message-ai'><img class='avatar' src=${bot} /><span id=${uniqueId}>${' '}</span></div>`;
+  chatContainer.innerHTML += `<div class='message message-ai'><div class='message__body' id=${uniqueId}></div><div class='message__footer'><span class='message__authoring'>Codex</span> - </div></div>`;
 
   // to focus scroll to the bottom
   chatContainer.scrollTop = chatContainer.scrollHeight;
@@ -55,16 +70,16 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
 
   // messageDiv.innerHTML = "..."
-  loader(messageDiv);
+  // loader(messageDiv);
 
-  const response = await fetch('https://codex-17jb.onrender.com', {
+  const response = await fetch('http://localhost:5005/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ prompt: data.get('prompt') }),
+    body: JSON.stringify({ prompt: data.get('message') }),
   });
 
   clearInterval(loadInterval);
-  messageDiv.innerHTML = ' ';
+  // messageDiv.innerHTML = ' ';
 
   if (response.ok) {
     const data = await response.json();
@@ -79,6 +94,7 @@ const handleSubmit = async (e) => {
 
 form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => {
+  console.log('EVENT LISTENER (keyup)');
   if (e.keyCode === 13) handleSubmit(e);
 });
 
