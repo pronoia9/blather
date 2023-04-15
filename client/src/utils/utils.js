319b3b9de35a078c57dd59a79583d68a03b71108
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-15 21:03:32 +0300

    copy functions from prev script.js

---
+++ client/src/utils/utils.js
@@ -0,0 +1,43 @@
+// generate unique ID for each message div of bot necessary for typing text effect for that specific reply without unique ID, typing text will work on every element
+export default function generateUniqueId() {
+  const timestamp = Date.now(),
+    randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+export default function getDate(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return ` - ${strTime}`;
+}
+
+// The ... loading text in AIs message while fetching response
+export default function loader(element) {
+  loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
+    element.textContent += '.';
+    // If the loading indicator has reached three dots, reset it
+    if (element.textContent === ' ....') element.textContent = ' ';
+  }, 300);
+}
+
+// AI typing text letter by letter
+export default function typeText(element, text, uniqueId) {
+  let index = 0;
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+      let timestamp = document.querySelector(`[id='${uniqueId}'] .message__authoring`);
+      timestamp.innerHTML += getDate(new Date());
+    }
+  }, 10);
+}
