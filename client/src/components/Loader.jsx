ff6d8b3b9a0d7007ab48452717d55de121b8dd51
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-15 19:45:16 +0300

    add loader component

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import './Loader.css';
+
+const Loader = () => {
+  return (
+    <div id='loading-wrapper'>
+      <div id='loading-text'>LOADING</div>
+      <div id='loading-content'></div>
+    </div>
+  );
+};
+
+export default Loader;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,6 @@ import Messages from './Messages';
 import Navbar from './Navbar';
 import SidebarLeft from './SidebarLeft';
 import SidebarRight from './SidebarRight';
+import Loader from './Loader';
 
-export { Messages, Navbar, SidebarLeft, SidebarRight };
+export { Loader, Messages, Navbar, SidebarLeft, SidebarRight };
