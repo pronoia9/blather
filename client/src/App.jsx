9bd23bc0c59ad8c3427144c1b57b2079e09b1022
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-15 19:45:39 +0300

    add poem fetch + loading screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,57 @@
-import { Messages, Navbar, SidebarLeft, SidebarRight } from './components';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { Loader, Messages, Navbar, SidebarLeft, SidebarRight } from './components';
 
 function App() {
-  return (
-    <div className='app-skeleton'>
-      <Navbar />
+  const [tags, setTags] = useState('');
+  const [links, setLinks] = useState('');
+  const [poem, setPoem] = useState('');
+  const [loading, setLoading] = useState(!(poem.length && tags.length && links.length));
 
-      <div className='app-container'>
-        <SidebarLeft />
-        <Messages />
-        <SidebarRight />
-      </div>
-    </div>
+  useEffect(() => {
+    console.log('poem', poem.length);
+    console.log('tags', tags.length);
+    console.log('links', links.length);
+    setLoading(!(poem.length || tags.length || links.length));
+  }, [tags, links, poem])
+
+  // TAGS
+
+  // LINKS
+  
+  // POEM
+  async function getPoem() {
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_URL,
+        { prompt: 'Give me a random short poem' },
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+      setPoem(`\n${response.data.bot.trim()}`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    getPoem();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className='app-skeleton'>
+          <Navbar />
+          <div className='app-container'>
+            <SidebarLeft tags={tags} links={links} />
+            <Messages />
+            <SidebarRight poem={poem} />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
