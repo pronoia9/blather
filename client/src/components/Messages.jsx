33582aba92d9788e2f0ed088bb2b764a4b32d467
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-16 02:40:39 +0300

    rename getDate, readd addMessage

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -2,51 +2,73 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import MessagesTopBar from './MessagesTopBar';
-import { generateUniqueId, getDate, loader, typeText } from '../utils/utils';
+import { generateUniqueId, getTimestamp, loader, typeText } from '../utils/utils';
 
 const Message = ({ id, message, from, time }) => {
   return (
     <div className={`message${from == 'Codex' ? ' message-ai' : ''}`} id={id}>
       <div className='message__body'>{message}</div>
-      <div className='message__footer'><span className='message__authoring'>{from}{time}</span></div>
+      <div className='message__footer'>
+        <span className='message__authoring'>
+          {from}
+          {time}
+        </span>
+      </div>
     </div>
   );
 };
 
-const hardMessages = [
-  { id: 'asdq3edsadfq13r', from: 'An Awesome User', message: 'Hi, how are you doing?', time: ' - 10:30 PM' },
-  { id: '51dq3edadfq13r', from: 'Codex', message: 'Stop bothering me with useless questions.', time: ' - 10:31 PM' },
-];
-
 const Messages = () => {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState(hardMessages);
+  const [messages, setMessages] = useState([
+    { id: 'asdq3edsadfq13r', from: 'An Awesome User', message: 'Hi, how are you doing?', time: ' - 10:30 PM' },
+    { id: '51dq3edadfq13r', from: 'Codex', message: 'Stop bothering me with useless questions.', time: ' - 10:31 PM' },
+  ]);
+  const [lastUID, setLastUID] = useState('');
+  const [loading, setLoading] = useState(false);
   let loadInterval;
 
-  const addMessage = (id, from, message, time) => { setMessages([...messages, { id, from, message, time }]) };
+  const addMessage = (id, from, message, time) => {
+    setMessages((messages) => [...messages, { id, from, message, time }]);
+  };
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // Return if there's no input
     if (!input.trim().length) {
       alert('You gotta type something you know...');
       return;
     }
 
-    // Users message
-    addMessage(generateUniqueId(), 'An Awesome User', input, getDate(new Date()));
+    // Add user's message
+    addMessage(generateUniqueId(), 'An Awesome User', input, getTimestamp(new Date()))
     // Reset user input/textarea
     setInput('');
-
-    // Save bots unique id
+    
+    
+    // Save the bot's unique id
     const uniqueId = generateUniqueId();
+    setLastUID(uniqueId);
     // Add empty message for bot
     addMessage(uniqueId, 'Codex', ' ', '');
+
     // Do the . . . loading/typing for bot
-    console.log(messages.find((msg) => msg.id === uniqueId));
     // loader()
+    // console.log(messages);
   };
 
-  useEffect(() => { console.log(messages) }, [messages]); // log messages when theres a change (DEVELOPMENT ONLY)
+  useEffect(() => {
+    console.log('messages (UE)', messages);
+    console.log('last uid (UE)', lastUID);
+
+    if (lastUID) {
+      const x = messages.find((msg) => msg.id == lastUID);
+      console.log('2nd UE', x);
+      // setMessages([...messages, { ...x, message: '...' }]);
+
+    }
+  }, [messages]);
 
   return (
     <div className='app-main'>
@@ -57,7 +79,7 @@ const Messages = () => {
         {/* Messages Area */}
         <div id='chat_container' className='channel-feed__body'>
           {messages.map((msg) => (
-            <Message key={msg.id} {...msg}  />
+            <Message key={msg.id} {...msg} />
           ))}
         </div>
 
@@ -76,7 +98,9 @@ const Messages = () => {
                   placeholder='Ask Codex...'
                   value={input}
                   onChange={(e) => setInput(e.target.value)}
-                  onKeyDown={(e) => { if (e.keyCode == 13 && !e.shiftKey) handleSubmit(e); }}></textarea>
+                  onKeyDown={(e) => {
+                    if (e.keyCode == 13 && !e.shiftKey) handleSubmit(e);
+                  }}></textarea>
               </div>
             </div>
             <div className='form-footer'>
