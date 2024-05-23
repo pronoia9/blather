7795ed5b671243930ce6a15b1eae82ccdd92329b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-05-23 13:45:41 -0400

    update prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   semi: false,
   useTabs: false,
   singleQuote: true,
-  arrowParens: 'avoid',
+  // arrowParens: 'avoid',
   tabWidth: 2,
   trailingComma: 'none',
   importOrder: [
@@ -30,5 +30,6 @@ module.exports = {
   importOrderBuiltinModulesToTop: true,
   importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
   importOrderMergeDuplicateImports: true,
-  importOrderCombineTypeAndValueImports: true
+  importOrderCombineTypeAndValueImports: true,
+  printWidth: 150,
 }
