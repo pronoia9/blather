1dc38c897d54ca1c3b42f93190b0ad492ed75b2d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-05-23 14:35:01 -0400

    revamp components ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils'
 import { TailwindIndicator } from '@/components/tailwind-indicator'
 import { Providers } from '@/components/providers'
 import { Header } from '@/components/header'
-import { Toaster } from '@/components/ui/sonner'
+import { Toaster } from '@/components/ui'
 
 export const metadata = {
   metadataBase: process.env.VERCEL_URL
