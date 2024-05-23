3018da819e03b8fa7c9c939ce19346240583fe55
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-05-23 15:49:39 -0400

    update gradient bg classes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
-        gradient: 'gradient-bg',
+        gradient: 'gradient-clr',
         'gradient-outline': 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground gradient-outline',
       },
       size: {
