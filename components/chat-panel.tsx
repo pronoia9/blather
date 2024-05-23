4816cdd752fb1edf5668b3fb3d454763e81c4c42
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-05-23 17:19:33 -0400

    change prompt panel bgs

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -87,7 +87,7 @@ export function ChatPanel({ id, title, input, setInput, isAtBottom, scrollToBott
           </div>
         ) : null}
 
-        <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
+        <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4 gradient-fg">
           <PromptForm input={input} setInput={setInput} />
           <FooterText className="hidden sm:block" />
         </div>
