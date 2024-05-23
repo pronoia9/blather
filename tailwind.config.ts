59f7b2bdabbfe8535c10b651c991e9679fccc1ab
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-05-23 15:04:38 -0400

    format

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}'
-  ],
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   prefix: '',
   theme: {
     container: {
@@ -54,7 +49,7 @@ module.exports = {
         card: {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))'
-        }
+        },
       },
       borderRadius: {
         lg: 'var(--radius)',
