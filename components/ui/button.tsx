f17c504ab17717999245ea529ee582bd73f30322
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-05-23 17:36:58 -0400

    add gradient outline + border variants styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,8 +15,10 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
-        gradient: 'gradient-clr',
+        'gradient': 'gradient-clr',
         'gradient-outline': 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground gradient-outline',
+        'gradient-border': 'gradient-border',
+        'gradient-ghost': 'hover:bg-accent hover:text-accent-foreground gradient-border'
       },
       size: {
         default: 'h-9 px-4 py-2',
