7a3f73be797ec6066299fc3087a9b75fd0d1a863
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-05-26 20:13:02 -0400

    add gradient-outline-bg

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -4,7 +4,7 @@ export function EmptyScreen() {
   return (
     <>
       <div className="mx-auto max-w-2xl px-4">
-        <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
+        <div className="flex flex-col gap-2 rounded-lg border p-8 gradient-outline-bg">
           <h1 className="text-lg font-semibold">{welcomeMessage.title}</h1>
           {welcomeMessage?.subtitles.map((text, i) => (
             <p key={`welcome-msg-${i}`} className="leading-normal text-muted-foreground">
