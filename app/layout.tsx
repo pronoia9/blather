33aa182992b44b0ac2c3273d429f82c8cf1f2ece
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2024-06-09 10:43:02 -0400

    justify main to center

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <Providers attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex flex-col flex-1 gradient-bg">{children}</main>
+            <main className="flex flex-col flex-1 gradient-bg justify-center">{children}</main>
           </div>
         </Providers>
       </body>
